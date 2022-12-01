444c19d83e1666a35307c08bfee37dee9a57e4e3
dojowahi <33106883+dojowahi@users.noreply.github.com>
2022-12-01 13:13:40 -0600

    Create deploy_cf.sh

---
+++ deploy_cf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#####################################################################################################
+# Script Name: deploy_cf.sh
+# Date of Creation: 12/01/2022
+# Author: Ankur Wahi
+# Updated: 12/01/2022
+#####################################################################################################
+
+source ./config.sh
+
+project_id=${PROJECT_ID}
+cf_docai="docai-parser"
+
+
+echo "Deploying Doc AI CF"
+
+cd ~/docai-on-bigquery/src/cloud-functions/docai
+
+gcloud functions deploy ${cf_docai} --entry-point get_doc --runtime python39 --trigger-http --allow-unauthenticated --project ${project_id} --service-account ${ee_sa} --gen2 --region ${REGION} --run-service-account ${doc_sa} --memory 256MB
+
+
+
+endpoint_docai=$(gcloud functions describe ${cf_docai} --region=${REGION} --gen2 --format=json | jq -r '.serviceConfig.uri')
+
+
+bq mk -d docai
+
+
+build_sql="CREATE OR REPLACE FUNCTION docai.doc_extractor(uri STRING, mime_type STRING, location STRING,docai_processorId STRING) RETURNS STRING REMOTE WITH CONNECTION \`${project_id}.us.gcf-docai-conn\` OPTIONS ( endpoint = '${endpoint_docaii}')"
+
+    
+bq query --use_legacy_sql=false ${build_sql}
