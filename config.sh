3b1b05d497221cb3abb8e87e56b24d60b8da1792
dojowahi <33106883+dojowahi@users.noreply.github.com>
2022-12-01 13:03:55 -0600

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,21 @@
+##################################################
+##
+## Set these Variables
+##
+##################################################
+# existing GCP user that will:
+# create the project
+# attach a billing id (needs to have permission)
+# and provision resources
+export USER_EMAIL=<insert gcp user email>
+
+# project id for your NEW GCP project
+export PROJECT_ID=<insert project id>
+
+
+##################################################
+#Example
+##################################################
+# export USER_EMAIL=myuser@mydomain.com
+# export PROJECT_ID=docai-on-gcp
+##################################################
