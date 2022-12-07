7801eb4a9ad07963242f51955e1484fab95031ed
Ankur Wahi <33106883+dojowahi@users.noreply.github.com>
2022-12-06 18:10:19 -0600

    Update and rename setup_argolis_policy.sh to setup_org_policy.sh
    Consolidated policies

--- setup_org_policy.sh
+++ setup_org_policy.sh
@@ -20,7 +20,7 @@ echo "Creating new project"
 
 gcloud projects create ${PROJECT_ID}
 
-echo "Set default project"
+echo "Setting default project"
 
 gcloud config set project ${PROJECT_ID}
 
@@ -32,7 +32,7 @@ gcloud config set project ${PROJECT_ID}
 
 echo "Assigning billing account"
 
-gcloud beta billing projects link ${PROJECT_ID} --billing-account=${BILLING_ACCOUNT_ID}
+gcloud billing projects link ${PROJECT_ID} --billing-account=${BILLING_ACCOUNT_ID}
 
 ##################################################
 ##
@@ -40,53 +40,29 @@ gcloud beta billing projects link ${PROJECT_ID} --billing-account=${BILLING_ACCO
 ##
 ##################################################
 
-echo "configuring org policies at project level"
+echo "Configuring org policies at project level"
 
-#enable VPC peering, disabled in argolis
+#enable VPC peering
 cat <<EOF > new_policy.yaml
 constraint: constraints/compute.restrictVpcPeering
 listPolicy:
     allValues: ALLOW
-EOF
-gcloud resource-manager org-policies set-policy \
-    --project=${PROJECT_ID} new_policy.yaml
-
-#disable the shielded vm requirement, enabled in argolis
-gcloud resource-manager org-policies disable-enforce \
-    compute.requireShieldedVm --project=${PROJECT_ID}
-
-#allow external IPs for app engine, disabled in argolis
-    cat <<EOF > new_policy.yaml
 constraint: constraints/compute.vmExternalIpAccess
 listPolicy:
     allValues: ALLOW
-EOF
-gcloud resource-manager org-policies set-policy  \
-    --project=${PROJECT_ID} new_policy.yaml
-    
-#enable Cloud Function
-cat <<EOF > new_policy.yaml
 constraint: constraints/cloudfunctions.allowedIngressSettings
 listPolicy:
     allValues: ALLOW
-EOF
-gcloud resource-manager org-policies set-policy \
-    --project=${PROJECT_ID} new_policy.yaml
-    
-cat <<EOF > new_policy.yaml
 constraint: constraints/iam.allowedPolicyMemberDomains
 listPolicy:
     allValues: ALLOW
-EOF
-gcloud resource-manager org-policies set-policy \
-    --project=${PROJECT_ID} new_policy.yaml    
-    
-    
-#enable Key creation
-cat <<EOF > new_policy.yaml
 constraint: constraints/iam.disableServiceAccountKeyCreation
 boolean_policy:
     enforced: false
 EOF
 gcloud resource-manager org-policies set-policy \
     --project=${PROJECT_ID} new_policy.yaml
+
+#disable the shielded vm requirement
+gcloud resource-manager org-policies disable-enforce \
+    compute.requireShieldedVm --project=${PROJECT_ID}
